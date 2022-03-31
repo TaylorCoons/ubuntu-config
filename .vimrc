bd8d484bbc600bc1381d767b4ffb161cac3e73e4
Taylor Coons <taylorcoons8798@gmail.com>
2022-03-30 23:23:01 -0600

    Initial setup

---
+++ .vimrc
@@ -0,0 +1,29 @@
+set nocompatible
+
+set number
+
+filetype on
+
+filetype indent on
+
+syntax on
+
+set cursorline
+
+set cursorcolumn
+
+set shiftwidth=4
+
+set tabstop=4
+
+set expandtab
+
+set nowrap
+
+set incsearch
+
+set hlsearch
+
+set history=1000
+
+inoremap jj <esc>
---
+++ fresh.bash
@@ -0,0 +1,23 @@
+# preamble stuff
+echo "# Aliases" > alias.bash
+
+# ---- vim ----
+# install
+sudo apt install vim
+
+#configure
+cp .vimrc ~/.vimrc
+
+# ---- xclip ----
+# install
+sudo apt install xclip
+
+# alias
+echo "alias clipboard='xclip -sel clip'" >> alias.bash
+
+# ---- git ----
+# install
+sudo apt install git
+
+# configure
+git config --global init.defaultBranch main
